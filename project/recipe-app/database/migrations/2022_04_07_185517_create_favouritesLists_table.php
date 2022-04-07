567822cabf82c7a55ed897a7f0e9d8c26eb0a730
frinica <frida.nicander@chasacademy.se>
2022-04-07 21:21:27 +0200

    update logic in AuthController, update FavouriteList model and migration

--- project/recipe-app/database/migrations/2022_04_07_185517_create_favouritesLists_table.php
+++ project/recipe-app/database/migrations/2022_04_07_185517_create_favouritesLists_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('favourites-lists', function (Blueprint $table) {
+        Schema::create('favouritesLists', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('recipe_id');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('favourites-lists');
+        Schema::dropIfExists('favouritesLists');
     }
 };
