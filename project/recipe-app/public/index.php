c6a7f80da674cb31b78966588f5bda7d2d25a0ec
frinica <frida.nicander@chasacademy.se>
2022-04-13 12:59:40 +0200

    update proxies in middleware and remove useless code in index

--- project/recipe-app/public/index.php
+++ project/recipe-app/public/index.php
@@ -53,7 +53,3 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
